590af3edb581b22d1fe339e8dc0520a48c938d7c
nehabht <ne.bhatti2@icloud.com>
2022-03-29 15:33:11 +0200

    add email check

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,27 @@
+// esercizio email
+
+
+// chiedi la mail all'user
+
+const insert_mail = prompt("inserisci email")
+
+
+// arrey con le email registrate
 
 const mail_list = ["smith@gmail.com" , "my@gmail.com", "rossi@gmail.com"]
 
-console.log(mail_list)
+// log singole mail
 
+for (let i = 0; i < mail_list.length; i++){
+    let user_mail = mail_list[i];
+    
+    if (user_mail === insert_mail ){
+        console.log("sei dentro");
+    }else {
+        console.log("riprova")
+    }
+    
+}
+
+
+// esercizio dadi
