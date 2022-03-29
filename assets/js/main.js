d2f8d194a926a1fcae39df8cf292395aa75d888a
nehabht <ne.bhatti2@icloud.com>
2022-03-29 15:53:16 +0200

    add dadi game

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 // esercizio email
 
 
-// chiedi la mail all'user
+// ------- chiedi la mail all'user
 
-const insert_mail = prompt("inserisci email")
+// const insert_mail = prompt("inserisci email")
 
 
 // arrey con le email registrate
@@ -12,16 +12,46 @@ const mail_list = ["smith@gmail.com" , "my@gmail.com", "rossi@gmail.com"]
 
 // log singole mail
 
-for (let i = 0; i < mail_list.length; i++){
-    let user_mail = mail_list[i];
+// for (let i = 0; i < mail_list.length; i++){
+//     let user_mail = mail_list[i];
     
-    if (user_mail === insert_mail ){
-        console.log("sei dentro");
-    }else {
-        console.log("riprova")
-    }
+//     if (user_mail === insert_mail ){
+//         console.log("sei dentro");
+//     }else {
+//         console.log("riprova")
+//     }
     
+// }
+
+
+
+
+
+
+// --------esercizio dadi 
+
+// genera numero random per il giocatore
+
+const playerElement = Math.floor(Math.random() * 6) + 1;
+
+console.log(playerElement)
+
+
+// genera numero random per il computer
+
+const computerElement = Math.floor(Math.random() * 6) + 1;
+
+console.log(computerElement)
+
+
+// determinare chi ha vinto
+
+if (playerElement == computerElement) {
+    console.log("pareggio");
+} else if (playerElement < computerElement){
+    console.log("vince il computer");
+} else {
+    console.log("vince il giocatore")
 }
 
 
-// esercizio dadi
